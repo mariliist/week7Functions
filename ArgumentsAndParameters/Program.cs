294e48531b94b65d470e5c06664f570c2222938c
Mariliis Toon <mariliistoon@gmail.com>
2024-01-07 16:17:33 +0200

    commit

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a word");
+string userInout = Console.ReadLine();
+
+PrintAnyWord(userInout);
+
+static void PrintAnyWord(string anyString) // anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
